9f10e4a66208886b90cb0beb1721412bde93c0c3
yzmar4real <70125095+yzmar4real@users.noreply.github.com>
2020-12-10 19:48:17 +0400

    Delete Master.py
    Updated code is now L1_combined

--- Master.py
+++
@@ -1,6 +0,0 @@
-
-#  This script acts as a job script function to run both IOS/IOXE and NXOS HealthCheck Scripts
-#  It can be modified to perform different actions such as email out the output, cleanup the files for next rerun, compare output from previous dates,etc. 
-
-import Main_nxos
-import Main_ios
